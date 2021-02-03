4339a17934bbabf62588ffa174105be46020b80d
Victor Armegioiu <victor.armegioiu@gmail.com>
2021-02-03 14:26:03 +0100

    Add support for cross entropy approximation with sliced score estimation.

--- prior_utils.py
+++ prior_utils.py
@@ -43,38 +43,42 @@ def ComputeCrossEntropy(config):
     cross_entropy = None
 
     if 'gp' in method:
-        x, y = config['x'], config['y']
-        kernel_function = config['kernel_function']
-        kernel_matrix = kernel_function(tf.cast(x, tf.float64))
-        
-        # Perturb eigenvalues of the kernel matrix to ensure Cholesky
-        # decomposition succeeds.
-        kernel_matrix += 0.01 * tf.eye(tf.shape(x)[0], dtype=tf.float64)
+      x, y = config['x'], config['y']
+      kernel_function = config['kernel_function']
+      kernel_matrix = kernel_function(tf.cast(x, tf.float64))
+      kernel_matrix += 0.01 * tf.eye(tf.shape(x)[0], dtype=tf.float64)
 
-        # Build the GP as a simple MVN with the given `kernel_matrix`.
-        prior = tfp.distributions.MultivariateNormalFullCovariance(
-          tf.zeros(x.shape[0], dtype=tf.float64), kernel_matrix)
+      # Build the GP as a simple MVN with the given `kernel_matrix`.
+      prior = tfp.distributions.MultivariateNormalFullCovariance(
+        tf.zeros(x.shape[0], dtype=tf.float64), kernel_matrix)
 
-        # Compute analytic crossentropy.
-        cross_entropy = -tf.reduce_mean(
-          prior.log_prob(tf.cast(y, tf.float64)))
+      # Compute analytic crossentropy.
+      cross_entropy = -tf.reduce_mean(
+        prior.log_prob(tf.cast(y, tf.float64)))
           
     elif 'ssge' in method:
-        y = config['y']
-        n_particles = config['n_particles']
-        prior_particles = config['prior_particles']
-        estimator = config['estimator']
+      y = config['y']
+      n_particles = config['n_particles']
+      prior_particles = config['prior_particles']
+      estimator = config['estimator']
 
-        # Use SSGE to evaluate the gradient of the log prior
-        # on the predictions.
-        cross_entropy_gradients = estimator.compute_gradients(
-              np.tile(prior_particles, (n_particles, 1))[..., None],
-              y)
+      # Use SSGE to evaluate the gradient of the log prior
+      # on the predictions.
+      cross_entropy_gradients = estimator.compute_gradients(
+            np.tile(prior_particles, (n_particles, 1))[..., None], y)
 
-        cross_entropy = -tf.reduce_mean(
-              tf.reduce_sum(
-                  tf.stop_gradient(cross_entropy_gradients)
-                  * tf.cast(y, tf.float64), -1))
+      cross_entropy = -tf.reduce_mean(
+            tf.reduce_sum(
+                tf.stop_gradient(cross_entropy_gradients)
+                * tf.cast(y, tf.float64), -1))
 
+    elif 'sliced' in method:
+      y = config['y']
+      score_estimator = config['score_estimator']
+      cross_entropy_gradients = score_estimator(y)
+
+      cross_entropy = -tf.reduce_mean(tf.reduce_sum(
+        tf.stop_gradient(cross_entropy_gradients) * y))
+      
     return cross_entropy
 
