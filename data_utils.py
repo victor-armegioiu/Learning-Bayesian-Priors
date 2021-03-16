a737f136d739de992e24f3d8e8b64d0545909aec
Victor Armegioiu <victor.armegioiu@gmail.com>
2021-03-16 22:04:33 +0100

    Update data generating API.
    Add support for generating meta-trainig/tuning tasks that we can use for functional prior learning.

--- data_utils.py
+++ data_utils.py
@@ -3,65 +3,52 @@ import sklearn
 
 
 class UnknownDataGenerationError(Exception):
-    pass
+  pass
 
 
-def GetSinusoidParams(task_count):
-    """Parameters for fa,b,c,β(x) = β ∗ x + a ∗ sin(1.5 ∗ (x − b)) + c.
+def GetSinusoidParams():
+  """Parameters for fa,b,c,β(x) = β ∗ x + a ∗ sin(1.5 ∗ (x − b)) + c.
+  """
+  a = np.random.uniform(0.7, 1.3)
+  b = np.random.normal(0, 0.1)
+  c = np.random.normal(5, 0.1)
+  beta = np.random.normal(0.5, 0.2)
 
-    Regressand is calculated as a function of the regressor
-    and the given fixed params.
-
-    @params:
-        task_count: Number of sets of hyperparameters to reproduce.
-    """
-    a = np.random.uniform(0.7, 1.3, task_count)
-    b = np.random.normal(0, 0.1, task_count)
-    c = np.random.normal(5, 0.1, task_count)
-    beta = np.random.normal(5, 0.2, task_count)
-
-    tasks_data = []
-    for i in range(task_count):
-        task_data = {'a': a[i], 'b': b[i], 'c': c[i], 'beta': beta[i]}
-        tasks_data.append(task_data)
-    return tasks_data
+  return a, b, c, beta
 
 
 def GetDataset(config):
-    """Generate synthetic data for small regression problems.
+  """Generate synthetic data for small regression problems.
 
-    Given a configuration of the required synthetic
-    data this method currently supports:
-        - Generation of regressors from an uniform distribution, where
-          regressands are computed as β ∗ x + a ∗ sin(1.5 ∗ (x − b)) + c.
-        - Generating a random regression problem.
-    """
+  Given a configuration of the required synthetic
+  data this method currently supports:
+      - Generation of regressors from an uniform distribution, where
+        regressands are computed as β ∗ x + a ∗ sin(1.5 ∗ (x − b)) + c.
+      - Generating a random regression problem.
+  
+  """
 
-    generation_method = config['generation_method']
-    size = config['size']
-    input_shape = config['input_shape']
+  generation_method = config['generation_method']
+  meta_task_cnt = config['meta_training_task_cnt']
+  meta_tuning_task_cnt = config['meta_tuning_task_cnt']
 
-    x_train, y_train = None, None
-    if 'sin' in generation_method:
-        tasks_data = config['tasks_data']
-        for task_data in tasks_data:
-            a, b, c, beta = (task_data['a'], task_data['b'],
-                             task_data['c'], task_data['beta'])
-            x = np.random.uniform(-5, 5, size)
-            y = beta * x + a * np.sin(1.5 * (x - b)) + c
+  task_size = config['task_size']
+  input_shape = config['input_shape']
+  all_training_tasks = []
 
-            if x_train is None:
-                x_train = x
-                y_train = y
-            else:
-                x_train = np.concatenate((x_train, x))
-                y_train = np.concatenate((y_train, y))
+  if 'sin' in generation_method:
+    for _ in range(meta_task_cnt + meta_tuning_task_cnt):
+      a, b, c, beta = GetSinusoidParams()
+      x = np.sort(np.random.uniform(-5, 5, task_size))[:, None]
+      y = beta * x + a * np.sin(1.5 * (x - b)) + c
+      all_training_tasks.append((x, y))
 
-    elif 'sklearn' in generation_method:
-        x_train, y_train = sklearn.datasets.make_regression(size,
-                                                            input_shape)
+  elif 'sklearn' in generation_method:
+    x, y = sklearn.datasets.make_regression(size, input_shape)
+    all_training_tasks.append((x, y))
+      
+  else:
+    raise UnknownDataGenerationError
 
-    else:
-        raise UnknownDataGenerationError
-
-    return x_train, y_train
+  return (all_training_tasks[:-meta_tuning_task_cnt],  
+          all_training_tasks[-meta_tuning_task_cnt:])
